c71919719aee1b8e28c738492d4671a1bb2ddc35
SunriseYDY <sunriseydy@gmail.com>
2018-09-14 18:26:29 +0800

    添加PAC

---
+++ GFWList2PAC/RUN-ME.sh
@@ -0,0 +1,14 @@
+#/bin/bash
+echo "************************************************************************************************"
+echo
+echo "Change gfwlist to pac.txt that can be used in windows' ssr"
+echo
+echo "You should first install python, if you not install, please try 'apt-get install python'"
+echo 
+echo "it will download gfwlist from https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt, please wait"
+echo
+echo "************************************************************************************************"
+python main.py  -f pac.txt -p "SOCKS5 127.0.0.1:1080" # --user-rule https://dl.sunriseydy.top/pac/user.rule
+echo
+echo "OK, next copy the pac.txt to your ssr's folder"
+echo 
